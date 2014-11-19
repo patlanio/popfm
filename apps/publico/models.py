8ef0fce2dc434cbaa9cc147b8e9ca11d03fd7408
Manuel Patlan <patlanio@outlook.com>
2014-11-19 13:21:35 -0700

    Cambios minimos en algunos MODELS
    Se omitieron en el commit anterior

--- apps/publico/models.py
+++ apps/publico/models.py
@@ -1,11 +1,12 @@
+#encoding:utf-8
 from django.db import models
 
 class Saludo(models.Model):
-	autor = models.CharField(max_length=32, verbose_name='Como te llamas?')
-	email = models.EmailField(verbose_name='Cual es tu correo?')
+	nombre = models.CharField(max_length=64)
+	correo = models.EmailField(verbose_name='Correo')
 	mensaje = models.TextField(max_length=200)
 	fecha_creacion = models.DateTimeField(auto_now_add = True)
-	leido = models.BooleanField(default = False, verbose_name='Marcar como leido', help_text='Marcar este mensaje como leido cuando se haya hecho la mencion al aire')
+	leido = models.BooleanField(default = False, help_text='Marcar este mensaje como leido cuando se haya hecho la mencion al aire')
 
 	def __str__(self):
-		return self.autor + " - " + self.email
+		return self.correo
